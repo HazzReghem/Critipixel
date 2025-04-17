44ac09cc951a3aad4f0eb9ea2610aeb5eaedd3e9
HazzReghem <thibault.roussel@gmx.com>
2025-04-17 11:05:15 +0200

    PHPStan errors solving 0.1

--- src/Doctrine/DataFixtures/TagFixtures.php
+++ src/Doctrine/DataFixtures/TagFixtures.php
@@ -9,16 +9,30 @@ use function array_fill_callback;
 
 final class TagFixtures extends Fixture
 {
+    // public function load(ObjectManager $manager): void
+    // {
+    //     $tags = array_fill_callback(
+    //         0,
+    //         25,
+    //         static fn (int $index): Tag => (new Tag)->setName(sprintf('Tag %d', $index))
+    //     );
+
+    //     array_walk($tags, [$manager, 'persist']);
+
+    //     $manager->flush();
+    // }
+
     public function load(ObjectManager $manager): void
     {
-        $tags = array_fill_callback(
-            0,
-            25,
-            static fn (int $index): Tag => (new Tag)->setName(sprintf('Tag %d', $index))
-        );
-
-        array_walk($tags, [$manager, 'persist']);
+        $tags = [];
+        for ($i = 0; $i < 25; $i++) {
+            $tag = new Tag();
+            $tag->setName(sprintf('Tag %d', $i));
+            $tags[] = $tag;
+            $manager->persist($tag);
+        }
 
         $manager->flush();
     }
+
 }
