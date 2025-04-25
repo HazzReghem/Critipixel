a3f0fcda781e8bc63f343a73a78e1983f89e1037
HazzReghem <thibault.roussel@gmx.com>
2025-04-25 09:37:36 +0200

    exntending panthertestcase

--- tests/Functional/FunctionalTestCase.php
+++ tests/Functional/FunctionalTestCase.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\Panther\PantherTestCase;
 
-abstract class FunctionalTestCase extends WebTestCase
+abstract class FunctionalTestCase extends PantherTestCase
 {
     // protected KernelBrowser $client;
 
